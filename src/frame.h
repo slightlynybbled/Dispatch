67f5620a6b392349e98463eaf5457e81aa6e124c
jjones <jjones@updsys.com>
2016-04-16 06:26:03 +0000

    Added comments

--- src/frame.h
+++ src/frame.h
@@ -8,6 +8,7 @@
 
 /**
  * Use to push an array element to be framed and sent.
+ *
  * @param data a byte-aligned array pointer
  * @param length the length of the array
  */
@@ -22,9 +23,38 @@ void FRM_push(uint8_t* data, uint16_t length);
  */
 uint16_t FRM_pull(uint8_t* data);
 
+/**
+ * Assigns the 'readable' function from the hardware
+ * access library.
+ *
+ * @param functPtr a function pointer to a function which tells how
+ * many bytes can be read from the channel input
+ */
 void FRM_assignChannelReadable(uint16_t (*functPtr)());
+
+/**
+ * Assigns the 'writeable' function from the hardware
+ * access library.
+ *
+ * @param functPtr a function pointer to a function which tells how
+ * many bytes can be written to the channel output
+ */
 void FRM_assignChannelWriteable(uint16_t (*functPtr)());
+
+/**
+ * Assigns the 'read' function from the hardware access library.
+ *
+ * @param functPtr a function pointer to a function which will read <length>
+ * bytes to the channel output from <data>
+ */
 void FRM_assignChannelRead(void (*functPtr)(void* data, uint16_t length));
+
+/**
+ * Assigns the 'write' function from the hardware access library.
+ *
+ * @param functPtr a function pointer to a function which will read <length>
+ * bytes from the channel input and into <data>
+ */
 void FRM_assignChannelWrite(void (*functPtr)(void* data, uint16_t length));
 
 #endif
