ca5b0ef1ad709398eb1a5daab1521b435e8bd130
jjones <jjones@updsys.com>
2016-04-16 00:13:29 +0000

    Moved test code into setup()

--- test/test/test_frame.c
+++ test/test/test_frame.c
@@ -6,7 +6,12 @@
 
 void setUp(void)
 {
+    FRM_assignChannelReadable(&readable);
+    FRM_assignChannelWriteable(&writeable);
+    FRM_assignChannelRead(&read8);
+    FRM_assignChannelWrite(&write8);
     
+    channel_reset();
 }
 
 void tearDown(void)
@@ -16,28 +21,26 @@ void tearDown(void)
 
 void test_init_readable(void)
 {
-    FRM_assignChannelReadable(&readable);
-    
+    TEST_ASSERT_NOT_NULL(channelReadableFunctPtr);
     TEST_ASSERT_EQUAL_INT(channelReadableFunctPtr,readable);
 }
 
 void test_init_writeable(void)
 {
-    FRM_assignChannelWriteable(&writeable);
-    
+    TEST_ASSERT_NOT_NULL(channelWriteableFunctPtr);
     TEST_ASSERT_EQUAL_INT(channelWriteableFunctPtr,writeable);
 }
 
 void test_init_read(void)
 {
-    FRM_assignChannelRead(&read8);
-    
+    TEST_ASSERT_NOT_NULL(channelReadFunctPtr);
     TEST_ASSERT_EQUAL_INT(channelReadFunctPtr,read8);
 }
 
 void test_init_write(void)
 {
-    FRM_assignChannelWrite(&write8);
-    
+    TEST_ASSERT_NOT_NULL(channelWriteFunctPtr);
     TEST_ASSERT_EQUAL_INT(channelWriteFunctPtr,write8);
 }
+
+
