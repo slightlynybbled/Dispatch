65b4c02bf45319ab68d09423461fc05df6d3c2b7
Jason R. Jones <slightlynybbled@gmail.com>
2016-04-19 23:29:43 +0000

    Added signed tests

--- test/test/test_dispatch.c
+++ test/test/test_dispatch.c
@@ -107,3 +107,28 @@ void test_publish_3x3_u8u16u32(void){
         TEST_ASSERT_EQUAL_INT(dataTest[i], writeData[i]);
     }
 }
+
+void test_publish_3x3_s8s16s32(void){
+    uint8_t dataTest[WRITE_DATA_LENGTH] = 
+                        {   START_OF_FRAME,
+                            'f', 'o', 'o', 0,
+                            3, 3, 0, 0x53, 0x07, 
+                            246, ESC ^ 0x20, 236, 226, // <= one of these happens to be an esc char
+                            216, 255, 206, 255, 196, 255,
+                            186, 255, 255, 255, 176, 255, 255, 255, 166, 255, 255, 255,
+                            234, 234,
+                            END_OF_FRAME
+                        };
+
+    int8_t data0[] = {-10, -20, -30};
+    int16_t data1[] = {-40, -50, -60};
+    int32_t data2[] = {-70, -80, -90};
+    
+    DIS_publish("foo:3,s8,s16,s32", data0, data1, data2);
+    
+    uint16_t i;
+    for(i = 0; i < 35; i++){
+        printf("%d ", i);
+        TEST_ASSERT_EQUAL_INT(dataTest[i], writeData[i]);
+    }
+}
