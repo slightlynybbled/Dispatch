60595e72b9054a53435acb96be67cef9be7e89c8
jjones <jjones@updsys.com>
2016-04-19 00:21:34 +0000

    Added string test

--- test/test/test_dispatch.c
+++ test/test/test_dispatch.c
@@ -5,6 +5,8 @@
 
 void setUp(void)
 {
+    DIS_init();
+    
     DIS_assignChannelReadable(&readable);
     DIS_assignChannelWriteable(&writeable);
     DIS_assignChannelRead(&read8);
@@ -40,4 +42,22 @@ void test_init_write(void)
 {
     TEST_ASSERT_NOT_NULL(channelWriteFunctPtr);
     TEST_ASSERT_EQUAL_INT(channelWriteFunctPtr,write8);
+}
+
+void test_publish_string(void){
+    uint8_t dataTest[WRITE_DATA_LENGTH] = 
+                        {   START_OF_FRAME,
+                            'f', 'o', 'o', 0,
+                            1, 3, 0, 1, 
+                            'b','a','r',
+                            128, 37,
+                            END_OF_FRAME
+                        };
+
+    DIS_publish("foo", "bar");
+    
+    uint16_t i;
+    for(i = 0; i < 15; i++){
+        TEST_ASSERT_EQUAL_INT(dataTest[i], writeData[i]);
+    }
 }
