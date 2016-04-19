992a71305eb3bbdd5c7f720bc126fda815c6fad8
jjones <jjones@updsys.com>
2016-04-19 00:54:14 +0000

    Added test cases for dispatch.c

--- test/test/test_dispatch.c
+++ test/test/test_dispatch.c
@@ -2,6 +2,7 @@
 #include "dispatch.c"
 #include "frame.c"
 #include "mock_uart_channel.c"
+#include <stdio.h>
 
 void setUp(void)
 {
@@ -58,6 +59,51 @@ void test_publish_string(void){
     
     uint16_t i;
     for(i = 0; i < 15; i++){
+        TEST_ASSERT_EQUAL_INT(dataTest[i], writeData[i]);
+    }
+}
+
+void test_publish_1_u8(void){
+    uint8_t dataTest[WRITE_DATA_LENGTH] = 
+                        {   START_OF_FRAME,
+                            'f', 'o', 'o', 0,
+                            1, 1, 0, 2, 
+                            10,
+                            83, 56,
+                            END_OF_FRAME
+                        };
+
+    uint8_t data[1] = {10};
+    DIS_publish("foo,u8", data);
+    
+    uint16_t i;
+    for(i = 0; i < 13; i++){
+        printf("%d ", i);
+        TEST_ASSERT_EQUAL_INT(dataTest[i], writeData[i]);
+    }
+}
+
+void test_publish_3x3_u8u16u32(void){
+    uint8_t dataTest[WRITE_DATA_LENGTH] = 
+                        {   START_OF_FRAME,
+                            'f', 'o', 'o', 0,
+                            3, 3, 0, 0x42, 0x06, 
+                            10, 20, 30,
+                            40, 0, 50, 0, 60, 0,
+                            70, 0, 0, 0, 80, 0, 0, 0, 90, 0, 0, 0,
+                            87, 250,
+                            END_OF_FRAME
+                        };
+
+    uint8_t data0[] = {10, 20, 30};
+    uint16_t data1[] = {40, 50, 60};
+    uint32_t data2[] = {70, 80, 90};
+    
+    DIS_publish("foo:3,u8,u16,u32", data0, data1, data2);
+    
+    uint16_t i;
+    for(i = 0; i < 34; i++){
+        printf("%d ", i);
         TEST_ASSERT_EQUAL_INT(dataTest[i], writeData[i]);
     }
 }
