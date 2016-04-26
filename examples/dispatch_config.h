618d9a11de89c336568b672c389b938662d55730
Jason R. Jones <slightlynybbled@gmail.com>
2016-04-26 16:09:49 -0400

    Changed buffer sizes

--- examples/dispatch_config.h
+++ examples/dispatch_config.h
@@ -2,19 +2,19 @@
 #define	DISPATCH_CONFIG_H
 
 /** The max number of dimensions that will be utilized */
-#define MAX_NUM_OF_FORMAT_SPECIFIERS    4
+#define MAX_NUM_OF_FORMAT_SPECIFIERS    1
 
 /** The maximum number of subscriptions that will be utilized */
-#define MAX_NUM_OF_SUBSCRIPTIONS        4
+#define MAX_NUM_OF_SUBSCRIPTIONS        2
 
 /** The maximum topic string length */
-#define MAX_TOPIC_STR_LEN               16
+#define MAX_TOPIC_STR_LEN               8
 
 /** The maximum transmission message length */
-#define MAX_TRANSMIT_MESSAGE_LEN        128
+#define MAX_TRANSMIT_MESSAGE_LEN        32
 
 /** The maximum receive message length */
-#define MAX_RECEIVE_MESSAGE_LEN         32
+#define MAX_RECEIVE_MESSAGE_LEN        32
 
 /** The received frame length */
 #define RX_FRAME_LENGTH 64
